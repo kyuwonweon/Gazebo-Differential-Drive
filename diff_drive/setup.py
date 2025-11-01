a4e966b9ca95577a6b023b25eeb50f1b21c551fb
kyuwon <kyuwon0917@gmail.com>
2025-11-01 12:36:49 -0500

    working gazebo

--- diff_drive/setup.py
+++ diff_drive/setup.py
@@ -1,4 +1,23 @@
+"""Setup file for gazebo world setting."""
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+
+def recursive_files(prefix, path):
+    """
+    Recurse over path returning a list of tuples.
+
+    :param prefix: prefix path to prepend to the path
+    :param path: Path to directory to recurse.
+    Path should not have a trailing '/'
+    :return: List of tuples. First element of each tuple is destination path,
+    second element is a list of files to copy to that path
+    """
+    return [(str(Path(prefix)/subdir),
+             [str(file) for file in subdir.glob('*') if not file.is_dir()])
+            for subdir in Path(path).glob('**')]
+
 
 package_name = 'diff_drive'
 
@@ -10,13 +29,18 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['env-hooks/diff_drive.dsv']),
+        *recursive_files('share/' + package_name, 'models'),
+        *recursive_files('share/' + package_name, 'worlds'),
+        *recursive_files('share/' + package_name, 'launch'),
+        *recursive_files('share/' + package_name, 'urdf')
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='kyuwon',
     maintainer_email='kyuwon0917@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Load gazebo world',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
